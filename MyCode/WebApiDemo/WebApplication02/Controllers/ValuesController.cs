a0da04c14fe87ffcc6c782222a726d32b60117bf
Wagner Xu <wagnerhsu@hotmail.com>
2022-08-16 14:53:29 +0800

    use logger name to only log specific information to appender

---
+++ MyCode/WebApiDemo/WebApplication02/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logger.LogInformation("ValueController.Get");
+            return "Values.Controller";
+        }
+    }
+}
